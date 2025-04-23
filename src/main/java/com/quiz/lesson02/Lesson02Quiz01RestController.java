128070484c299c5912268b6477db0f22090189b2
test <test@test.com>
2025-04-23 20:50:56 +0900

    2강 2번 문제 풀이 완료

---
+++ src/main/java/com/quiz/lesson02/Lesson02Quiz01RestController.java
@@ -0,0 +1,24 @@
+package com.quiz.lesson02;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.quiz.lesson02.domain.Store;
+import com.quiz.lesson02.service.StoreBO;
+
+
+@RestController  // @Controller + @ResponseBody
+public class Lesson02Quiz01RestController {
+	@Autowired
+	private StoreBO storeBO; // DI
+
+	// http://localhost:8080/lesson02/quiz01
+	@RequestMapping("/lesson02/quiz01")
+	public List<Store> quiz01() {
+		return storeBO.getStoreList(); // 응답(Response) -> JSON
+	}
+	
+}
---
+++ src/main/java/com/quiz/lesson02/service/StoreBO.java
@@ -0,0 +1,22 @@
+package com.quiz.lesson02.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson02.domain.Store;
+import com.quiz.lesson02.mapper.StoreMapper;
+
+@Service
+public class StoreBO {
+	
+	@Autowired
+	private StoreMapper storeMapper; // DI: spring bean 주입
+
+	// input: X
+	// output: List<Store>
+	public List<Store> getStoreList() {
+		return storeMapper.selectStoreList();
+	}
+}
