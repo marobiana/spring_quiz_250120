be8920383711251568889dd94df6f0198e68dc6e
test <test@test.com>
2025-04-30 21:45:04 +0900

    1-2번 풀이 완료

--- src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz01Controller.java
@@ -1,12 +1,22 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.quiz.lesson04.domain.Seller;
+import com.quiz.lesson04.service.SellerBO;
 
 @RequestMapping("/lesson04/quiz01")
 @Controller
 public class Lesson04Quiz01Controller {
+	
+	@Autowired
+	private SellerBO sellerBO;
 
 	// 추가 화면
 	// http://localhost:8080/lesson04/quiz01/add-seller-view
@@ -17,4 +27,30 @@ public class Lesson04Quiz01Controller {
 	
 	// 추가 기능
 	// /lesson04/quiz01/add-seller
+	@PostMapping("/add-seller")
+	public String addSeller(
+			@RequestParam("nickname") String nickname,
+			@RequestParam(value = "profileImageUrl", required = false) String profileImageUrl,
+			@RequestParam("temperature") double temperature) {
+		
+		sellerBO.addSeller(nickname, profileImageUrl, temperature);
+		
+		return "lesson04/afterAddSeller";
+	}
+	
+	// 최근 가입자 한명 정보 화면
+	// /lesson04/quiz01/seller-info-view
+	@GetMapping("/seller-info-view")
+	public String sellerInfoView(Model model) {
+		// db select
+		Seller seller = sellerBO.getLatestSeller();
+		
+		// model에 담기
+		model.addAttribute("seller", seller);
+		model.addAttribute("title", "판매자 정보");
+		
+		// 화면
+		return "lesson04/sellerInfo";
+	}
+	
 }
---
+++ src/main/java/com/quiz/lesson04/service/SellerBO.java
@@ -0,0 +1,28 @@
+package com.quiz.lesson04.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.quiz.lesson04.domain.Seller;
+import com.quiz.lesson04.mapper.SellerMapper;
+
+@Service
+public class SellerBO {
+	
+	@Autowired
+	private SellerMapper sellerMapper;
+
+	// input: 3개 파라미터
+	// output: X
+	public void addSeller(String nickname, 
+			String profileImageUrl, double temperature) {
+		
+		sellerMapper.insertSeller(nickname, profileImageUrl, temperature);
+	}
+	
+	// input: X
+	// output: Seller(단건 or null)
+	public Seller getLatestSeller() {
+		return sellerMapper.selectLatestSeller();
+	}
+}
