0c79322012a725e5f75806618b30ef30dcb72c5d
test <test@test.com>
2025-05-12 21:42:20 +0900

    문제1-1 풀이 완료

--- src/main/java/com/quiz/lesson06/Lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Controller.java
@@ -1,12 +1,17 @@
 package com.quiz.lesson06;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.quiz.lesson06.domain.Bookmark;
 import com.quiz.lesson06.service.BookmarkBO;
@@ -37,6 +42,23 @@ public class Lesson06Controller {
 	}
 	
 	// /add-bookmark
+	// AJAX 요청 - 즐겨찾기 추가
+	@ResponseBody
+	@PostMapping("/add-bookmark")
+	public Map<String, Object> addBookmark(
+			@RequestParam("name") String name,
+			@RequestParam("url") String url) {
+		
+		// db insert
+		bookmarkBO.addBookmark(name, url);
+		
+		// 성공 여부
+		Map<String, Object> result = new HashMap<>();
+		result.put("code", 200); // 성공
+		result.put("result", "성공");
+		
+		return result;
+	}
 }
 
 
