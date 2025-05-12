f4b5fde6aaff604151f466f22232567c51e9732f
test <test@test.com>
2025-05-12 19:50:43 +0900

    문제 1-1 목록 뿌리기 완료, 추가 스크립트 완료

--- src/main/java/com/quiz/lesson06/Lesson06Controller.java
+++ src/main/java/com/quiz/lesson06/Lesson06Controller.java
@@ -1,22 +1,46 @@
 package com.quiz.lesson06;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.lesson06.domain.Bookmark;
+import com.quiz.lesson06.service.BookmarkBO;
 
 @RequestMapping("/lesson06")
 @Controller
 public class Lesson06Controller {
+	
+	@Autowired
+	private BookmarkBO bookmarkBO;
 
-	// 즐겨찾기 목록 화면
+	// 1-2) 즐겨찾기 목록 화면
 	@GetMapping("/bookmark-list-view")
-	public String bookmarkListView() {
+	public String bookmarkListView(Model model) {
+		// DB select
+		List<Bookmark> bookmarkList = bookmarkBO.getBookmarkList();
+		
+		// model
+		model.addAttribute("bookmarkList", bookmarkList);
+		
 		return "lesson06/bookmarkList";
 	}
 	
-	// 즐겨찾기 추가 화면
+	// 1-1) 즐겨찾기 추가 화면
 	@GetMapping("/add-bookmark-view")
 	public String addBookmarkView() {
 		return "lesson06/addBookmark";
 	}
+	
+	// /add-bookmark
 }
+
+
+
+
+
+
