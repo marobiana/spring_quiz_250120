7b985055db70e65c8f63d30e146bfb395be5aa3f
test <test@test.com>
2025-05-14 20:51:57 +0900

    6강 마지막 문제 1번만 풀이, 2번 datepicker도

--- src/main/java/com/quiz/booking/BookingController.java
+++ src/main/java/com/quiz/booking/BookingController.java
@@ -1,17 +1,62 @@
 package com.quiz.booking;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.quiz.booking.bo.BookingBO;
+import com.quiz.booking.domain.Booking;
 
 @RequestMapping("/booking")
 @Controller
 public class BookingController {
 
+	@Autowired
+	private BookingBO bookingBO;
+	
 	// 예약 목록 화면
 	@GetMapping("/booking-list-view")
-	public String bookingListView() {
+	public String bookingListView(Model model) {
+		// select
+		List<Booking> bookingList = bookingBO.getBookingList();
+		
+		// model
+		model.addAttribute("bookingList", bookingList);
+		
 		return "booking/bookingList";
+	}
+	
+	// AJAX 요청 - delete booking
+	@ResponseBody
+	@DeleteMapping("/delete-booking")
+	public Map<String, Object> deleteBooking(
+			@RequestParam("id") int id) {
+		
+		// delete
+		int rowCount = bookingBO.deleteBookingById(id);
+		
+		// 응답값 json string
+		// {"code":200, "result":"성공"}
+		// {"code":500, "error_message":"삭제할 대상이 없습니다."}
+		Map<String, Object> result = new HashMap<>();
+		if (rowCount > 0) {
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else {
+			result.put("code", 500);
+			result.put("error_message", "삭제할 대상이 없습니다.");
+		}
+		
+		return result;
 	}
 	
 	// 예약하기 화면
